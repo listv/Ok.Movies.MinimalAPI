7e9cd39eec238e21dffb98a2f43ac4d25819fcfe
Oleksnadr Kuziakov <akuziakov@gmail.com>
2023-09-07 07:20:50 +0300

    Use script to bulk read github secrets

---
+++ .github/workflows/generate-env-file.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+
+# Fetch GitHub Secrets
+DATABASE__CONNECTIONSTRING="${{ secrets.DB_CONNECTION_STRING }}"
+#SECRET_2="${{ secrets.SECRET_2 }}"
+# Add more secrets as needed
+
+# Generate the environment file
+echo "DATABASE__CONNECTIONSTRING=$DATABASE__CONNECTIONSTRING" > generated-env-file.env
+#echo "SECRET_2=$SECRET_2" >> generated-env-file.env
+# Add more secrets as needed

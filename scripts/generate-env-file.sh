7e9569499678b60d4e604b53cd1e5eb5c09be131
Oleksnadr Kuziakov <akuziakov@gmail.com>
2023-10-01 06:28:39 +0300

    Add migration project params to github secrets

--- scripts/generate-env-file.sh
+++ scripts/generate-env-file.sh
@@ -2,10 +2,14 @@
 
 # Fetch GitHub Secrets
 DATABASE__CONNECTIONSTRING="$1"
+POSTGRES_PASSWORD="$2"
+POSTGRES_DB="$3"
 #SECRET_2="$2"
 # Add more secrets as needed
 
 # Generate the environment file
 echo "DATABASE__CONNECTIONSTRING=$DATABASE__CONNECTIONSTRING" > .env
+echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> .env
+echo "POSTGRES_DB=$POSTGRES_DB" >> .env
 #echo "SECRET_2=$SECRET_2" >> .env
 # Add more secrets as needed

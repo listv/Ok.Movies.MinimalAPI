cad363c813db58dbbfc6809aaad3b181b4208b9f
Oleksnadr Kuziakov <akuziakov@gmail.com>
2023-10-06 06:03:23 +0300

    Create ratings table.

---
+++ src/Database.Migrations/2023/202310060525_CreateRatingsTable.cs
@@ -0,0 +1,31 @@
+﻿using FluentMigrator;
+
+namespace Database.Migrations._2023;
+
+[Migration(202310060525)]
+public class CreateRatingsTable : Migration
+{
+    private const string RatingsTableName = "ratings";
+    private const string MoviesTableName = "movies";
+    private const string UserIdColumName = "user_id";
+    private const string MovieIdColumName = "movie_id";
+    private const string IdColumName = "id";
+    private const string RatingColumName = "rating";
+
+    public override void Up()
+    {
+        Create.Table(RatingsTableName)
+            .WithColumn(UserIdColumName).AsGuid()
+            .WithColumn(MovieIdColumName).AsGuid().ForeignKey(MoviesTableName, IdColumName)
+            .WithColumn(RatingColumName).AsInt32().NotNullable();
+
+        Create.PrimaryKey("PK_ratings_Id")
+            .OnTable(RatingsTableName)
+            .Columns(UserIdColumName, MovieIdColumName);
+    }
+
+    public override void Down()
+    {
+        Delete.Table(RatingsTableName);
+    }
+}
